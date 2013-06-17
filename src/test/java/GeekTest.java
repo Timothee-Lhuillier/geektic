32c9eb60790fc7aef7f91f81986ce1d33fca3d6a
Timothee-Lhuillier <timothee.lhuillier@sfr.fr>
2013-06-17 21:55:50 +0200

    refactoring GeekTest

--- src/test/java/GeekTest.java
+++ src/test/java/GeekTest.java
@@ -1,40 +1,32 @@
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.bugsprod.geektic.Geek;
-import com.bugsprod.geektic.GeekDao;
-import com.bugsprod.geektic.GeekService;
 import com.bugsprod.geektic.Interest;
 
 public class GeekTest {
 
-	Geek geek;
-	EntityManager mockedEM;
-	GeekDao mockedGD;
+	Interest java;
+	List<Interest> inters;
 	
 	@Before
 	public void setUp() {
-		geek = new Geek(0l, "Dupond", "paul");
-		mockedEM = mock(EntityManager.class);
-		mockedGD = mock(GeekDao.class);
+		java = new Interest(0, "java");
+		inters = new ArrayList<Interest>();
 	}
 	
 	@Test
 	public void equalsStringTestInterest() {
-		Interest inter = new Interest(0, "java");
-		assertEquals(true, inter.equals("java"));
+		assertEquals(true, java.equals("java"));
 	}
 	
 	@Test
 	public void equalsInterestTestInterest() {
-		Interest inter = new Interest(0, "java");
 		Interest inter2 = new Interest(1, "java");
-		assertEquals(true, inter.equals(inter2));
+		assertEquals(true, java.equals(inter2));
 	}
 }
